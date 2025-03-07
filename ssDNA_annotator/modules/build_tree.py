d42d01be79ea7c5805b25c2a6abbb7a0b144d48b
Ricardo RP Silva <ricrocha82@gmail.com>
2025-03-07 16:22:12 -0500

    update build_tree.py

--- ssDNA_annotator/modules/build_tree.py
+++ ssDNA_annotator/modules/build_tree.py
@@ -65,14 +65,15 @@ def sanitize_sequence_names(input_fasta, sanitized_fasta, name_table_file):
 #     AlignIO.write(alignment, phylip_file, "phylip")
 #     print(f"Converted {sanitized_fasta} to {phylip_file} in PHYLIP format.")
 
-def generate_phylogenetic_tree(input_fasta):
+def generate_phylogenetic_tree(input_fasta, bootstrap, threads, extra_args, model):
     """
     Build phylonetic tree using IQTREE
     Step 1: find the appropriate evolutionary model using ModelFinder (Kalyaanamoorthy et al., 2017) > version 1.5.4 and older, -m MFP is the default behavior.
     Step 2: generate tree using bootstrap support generated for 1,000 iterations
     """
 
-    cmd = f"iqtree2 -s {input_fasta} -B 1000 -T AUTO"
+    cmd = f"iqtree2 -s {input_fasta} -m {model} -B {bootstrap} -T {threads} {extra_args}"
+
     logging.info(f"Running phylogenetic tree generation with command: {cmd}")
     run_command(cmd, "Error building Phylogenetic tree")
 
@@ -81,6 +82,11 @@ def main():
     parser = argparse.ArgumentParser(description="Pipeline for sequence alignment, trimming, motif splitting, and phylogenetic tree generation.")
     parser.add_argument("-i","--input_fasta", required=True, help="Input FASTA file with sequences.")
     parser.add_argument("-d", "--directory", required=True, help="Directory containing input FASTA file and for saving outputs.")
+    parser.add_argument("-B", "--bootstrap", default=1000, type=int, help="Number of bootstrap iterations (default: 1000)")
+    parser.add_argument("-T", "--threads", default="AUTO", help="Number of threads to use (default: AUTO)")
+    parser.add_argument("-m", "--model", default="MFP", help="Substitution models (default: MFP - ModelFinder)")
+    parser.add_argument("--extra_args", default="", help="Extra arguments to pass directly to IQ-TREE")
+    
 
     args = parser.parse_args()
     prefix = os.path.splitext(os.path.basename(args.input_fasta))[0]
@@ -89,7 +95,8 @@ def main():
     log_file = os.path.join(output_dir, "build_tree.log")
     name_table_file = os.path.join(output_dir, f"{prefix}_sanitized_name_table.tsv")
     sanitized_fasta = os.path.join(output_dir, f"{prefix}_sanitized_sequences.fasta")
-
+    bootstrap = args.bootstrap
+    
     # Create nested directories
     try:
         os.makedirs(output_dir)
@@ -122,9 +129,7 @@ def main():
 
     logging.info("Starting pipeline")
     sanitize_sequence_names(input_fasta, sanitized_fasta, name_table_file)
-    generate_phylogenetic_tree(sanitized_fasta)
-    logging.info(f"Phylogenetic tree analysis complete. Outputs saved in {output_dir}")
-
+    generate_phylogenetic_tree(sanitized_fasta, args.bootstrap, args.threads, args.extra_args, args.model)
     print(f"Phylogenetic tree analysis complete. Outputs saved in {output_dir}")
 
 
@@ -152,4 +157,4 @@ if __name__ == "__main__":
 
 # /fs/project/PAS1117/ricardo/ssDNA_tool/ssDNA_annotator/modules/build_tree.py \
 #         -i /fs/project/PAS1117/ricardo/ssDNA_tool/test_data/output/sub_reps_aligned_trimmed_sequences.fasta \
-#         -d /fs/project/PAS1117/ricardo/ssDNA_tool/test_data/output/tree
+#         -d /fs/project/PAS1117/ricardo/ssDNA_tool/test_data
